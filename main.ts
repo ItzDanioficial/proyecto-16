abe8efa59357335134e2ed91f7a977611d117c1a
ItzDanioficial <105298358+ItzDanioficial@users.noreply.github.com>
2022-05-10 16:19:36 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+let número = randint(1, 2)
+let distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+if (distancia > 5) {
+    maqueen.motorStop(maqueen.Motors.All)
+    music.playMelody("C5 G C5 G C5 G C5 G ", 120)
+}
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+basic.pause(1000)
+maqueen.motorStop(maqueen.Motors.All)
+if (número == 1) {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+} else {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+}
